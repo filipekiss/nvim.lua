f566b6719640972104537f7eabf0699fa3906c82
Filipe Kiss <hello@filipekiss.com.br>
2022-04-14 15:47:14 +0200

    feat(autocmd): Create missing folders when saving

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
 Nebula = require("nebula")
+
+Nebula.augroup("CreateMissingFolder", {
+  {
+    events = {"BufWritePre", "FileWritePre"},
+    targets = { "*" },
+    modifiers = {"silent!"},
+    command = "call mkdir(expand('<afile>:p:h'), 'p')"
+  }
+})
+
 Nebula.init()
