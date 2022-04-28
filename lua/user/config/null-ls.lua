ead320f0ebca60bac763d83a68b335e3626f27da
Filipe Kiss <hello@filipekiss.com.br>
2022-04-28 13:23:39 +0200

    feat(null-ls): Configure eslint and eslint_d

--- lua/user/config/null-ls.lua
+++ lua/user/config/null-ls.lua
@@ -4,6 +4,7 @@ if not null_ls then
 	return
 end
 local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 return {
 	debug = true,
 	on_attach = require("user.plugins.lsp.handlers").on_attach,
@@ -12,7 +13,11 @@ return {
 		-- system and availbable in the $PATH
 		-- Right next to each plugin I added a comment with the command that you
 		-- can use to install these dependencies
-		formatting.prettier, -- volta install prettier
+		formatting.prettier.with({
+			prefer_local = "node_modules/.bin",
+		}), -- volta install prettier
+		formatting.eslint_d,
 		formatting.stylua, -- cargo install stylua
+		diagnostics.eslint,
 	},
 }
