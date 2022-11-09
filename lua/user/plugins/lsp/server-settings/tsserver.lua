0b649bbd125610f936c27aef54ea352666cb55ad
Filipe Kiss <hello@filipekiss.com.br>
2022-11-09 01:01:13 +0100

    feat(tsserver): Update import preferences
    I always want to use relative imports in a project but use shorter
    imports if going outside the project folder

--- lua/user/plugins/lsp/server-settings/tsserver.lua
+++ lua/user/plugins/lsp/server-settings/tsserver.lua
@@ -1,8 +1,28 @@
+local function organize_imports()
+	local params = {
+		command = "_typescript.organizeImports",
+		arguments = { vim.api.nvim_buf_get_name(0) },
+		title = "",
+	}
+	vim.lsp.buf.execute_command(params)
+end
+
 local tsserver = {
 	on_attach = function(client)
 		-- disable tsserver formatting, I'll use prettier with null-ls
 		client.resolved_capabilities.document_formatting = false
 	end,
+	init_options = {
+		preferences = {
+			importModuleSpecifierPreference = "project-relative",
+		},
+	},
+	commands = {
+		OrganizeImports = {
+			organize_imports,
+			description = "Organize Imports",
+		},
+	},
 }
 
 return tsserver
