80ff2fdd7b4c508919f06df0295048fdba958b7c
Filipe Kiss <hello@filipekiss.com.br>
2023-04-13 12:07:25 +0200

    feat: update _ module
    * move __P to _.P
    * use vim.print if nvim >= 0.9.0

--- lua/_/init.lua
+++ lua/_/init.lua
@@ -1,11 +1,19 @@
-_G.__P = function(v)
-	return vim.pretty_print(v)
-end
-
 _G._ = {}
 function _.TableConcat(t1, t2)
 	for i = 1, #t2 do
 		t1[#t1 + 1] = t2[i]
 	end
 	return t1
+end
+
+function _.unrequire(m)
+	package.loaded[m] = nil
+	_G[m] = nil
+end
+
+function _.P(v)
+	if vim.fn.has("nvim-0.9.0") then
+		return vim.print(v)
+	end
+	return vim.pretty_print(v)
 end
