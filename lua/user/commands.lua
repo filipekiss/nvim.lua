0216f4b6dbd004d76705764290316c8e87117764
Filipe Kiss <hello@filipekiss.com.br>
2023-04-17 14:18:43 +0200

    feat(commands): add commonly misstyped commands

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -7,3 +7,8 @@ add_command(
 	require("user.functions").toggle_fold_method,
 	{ desc = "Toggle fold method" }
 )
+
+-- I usually misstype these
+add_command("W", "w", { desc = "Save current buffer" })
+add_command("X", "x", { desc = "Save current buffer and quit neovim" })
+add_command("Q", "q", { desc = "Quit" })
