45ad2a8241c32e3035f0035b32c3b1579d541c82
Filipe Kiss <hello@filipekiss.com.br>
2023-04-26 12:44:32 +0200

    feat(editor): add harpoon plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -242,6 +242,30 @@ return {
 			},
 		},
 	},
+	-- harpoon
+	{
+		"https://github.com/ThePrimeagen/harpoon",
+		opts = {},
+		keys = {
+			{
+				"gH",
+				function()
+					local ui = require("harpoon.ui")
+					ui.toggle_quick_menu()
+				end,
+			},
+			{
+				"gh",
+				function()
+					if vim.v.count > 0 then
+						require("harpoon.ui").nav_file(vim.v.count)
+					else
+						require("harpoon.mark").toggle_file()
+					end
+				end,
+			},
+		},
+	},
 	-- genghis
 	{
 		"https://github.com/chrisgrieser/nvim-genghis",
