fe2fc5497eaff58758f2c2d94834c8e2d54daa21
Filipe Kiss <hello@filipekiss.com.br>
2023-04-28 13:39:41 +0200

    feat(lsp): add Format commands
    * just add some commands for a more pleasant and manageable formatting
      experience
    
    :Format - Formats the current buffer even if autoformat is off
    :FormatToggle - Toggles autoformat on or off for the current buffer
    :FormatOn - Turns autoformat on for the current buffer
    :FormatOff - Turns autoformat off for the current buffer

--- lua/plugins/lsp/format.lua
+++ lua/plugins/lsp/format.lua
@@ -1,27 +1,36 @@
 local Util = require("idle.util")
+local buffer_table = require("idle.helpers.table").buffer_table
 local M = {}
 
-M.autoformat = true
+local LspBuffer = buffer_table({
+	autoformat = "autoformat",
+})
 
-function M.toggle()
-	if vim.b.autoformat == false then
-		vim.b.autoformat = nil
-		M.autoformat = true
-	else
-		M.autoformat = not M.autoformat
+---Toggle autoformat on or off
+---@param next_autoformat? boolean If true, enable autoformat. If false, disable it. If nil, toggle
+function M.set_autoformat(next_autoformat)
+	local current_autoformat = LspBuffer.autoformat
+	if next_autoformat == true then
+		current_autoformat = false
+	elseif next_autoformat == false then
+		current_autoformat = true
 	end
-	if M.autoformat then
-		Util.info("Enabled format on save", { title = "Format" })
-	else
+	if current_autoformat == nil or current_autoformat == true then
+		LspBuffer.autoformat = false
 		Util.warn("Disabled format on save", { title = "Format" })
+	else
+		LspBuffer.autoformat = true
+		Util.info("Enabled format on save", { title = "Format" })
 	end
 end
 
-function M.format()
-	local buf = vim.api.nvim_get_current_buf()
-	if vim.b.autoformat == false then
+---Format the current file using LSP
+---@param force boolean If true, will run format even if autoformat is disabled
+function M.format(force)
+	if LspBuffer.autoformat == false and force ~= true then
 		return
 	end
+	local buf = vim.api.nvim_get_current_buf()
 	local ft = vim.bo[buf].filetype
 	local have_nls = #require("null-ls.sources").get_available(
 		ft,
@@ -29,6 +38,7 @@ function M.format()
 	) > 0
 
 	vim.lsp.buf.format(vim.tbl_deep_extend("force", {
+		async = false,
 		bufnr = buf,
 		filter = function(client)
 			if have_nls then
@@ -37,6 +47,9 @@ function M.format()
 			return client.name ~= "null-ls"
 		end,
 	}, {}))
+	if force then
+		Util.info("File formatted", { title = "LSP" })
+	end
 end
 
 function M.on_attach(client, buf)
@@ -54,12 +67,41 @@ function M.on_attach(client, buf)
 			group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
 			buffer = buf,
 			callback = function()
-				if M.autoformat then
-					M.format()
-				end
+				M.format(false)
 			end,
 		})
 	end
+
+	local create_commands =
+		require("idle.helpers.command").create_commands_from_spec_list
+	---@type IdleCommandSpecList
+	local commands = {
+		Format = {
+			command = function()
+				M.format(true)
+			end,
+			desc = "[LSP] Format current file",
+		},
+		FormatToggle = {
+			command = function()
+				M.set_autoformat()
+			end,
+			desc = "[LSP] Toggle autoformat for current buffer",
+		},
+	}
+	local format_commands_state = {
+		On = true,
+		Off = false,
+	}
+	for state, value in pairs(format_commands_state) do
+		commands["Format" .. state] = {
+			command = function()
+				M.set_autoformat(value)
+			end,
+			desc = "[LSP] Turn autoformat " .. string.lower(state),
+		}
+	end
+	create_commands(commands)
 end
 
 return M
